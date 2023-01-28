927af1be152b66035226cc71783a31218fb78622
grzeznx2 <grzeznx2@gmail.com>
2023-01-28 22:40:29 +0100

    Add advanced types

--- src/main.ts
+++ src/main.ts
@@ -20,21 +20,52 @@ import {
   Bool,
 } from 'snarkyjs';
 
+const c = console.log;
+
 await isReady;
 
 const num1 = UInt32.from(40);
 const num2 = UInt64.from(40);
 
 const num1EqualsNum2: Bool = num1.toUInt64().equals(num2);
-console.log(`num1 === num2 ${num1EqualsNum2.toString()}`);
-console.log(`Fields in num1:`, num1.toFields().length);
+c(`num1 === num2 ${num1EqualsNum2.toString()}`);
+c(`Fields in num1:`, num1.toFields().length);
 
 const signedNum1 = Int64.from(-3);
 const signedNum2 = Int64.from(45);
 
 const signedNumSum = signedNum1.add(signedNum2);
-console.log(`Fields in signedNumSum:`, signedNumSum.toFields().length);
+c(`Fields in signedNumSum:`, signedNumSum.toFields().length);
 
 const char1 = Character.fromString('s');
+const char2 = Character.fromString('g');
 
-console.log(`Fields in char1: ${char1.toFields().length}`);
+c(`Fields in char1: ${char1.toFields().length}`);
+
+const str1 = CircuitString.fromString('abc...xyz');
+c(`str1: ${str1}`);
+c(`Fields in str1: ${str1.toFields().length}`); //128
+
+const zkAppPrivateKey = PrivateKey.random();
+const zkAppPublicKey = zkAppPrivateKey.toPublicKey();
+
+const data1 = char2.toFields().concat(signedNumSum.toFields());
+const data2 = char2.toFields().concat(str1.toFields());
+
+const signature = Signature.create(zkAppPrivateKey, data2);
+
+const verifiedData1 = signature.verify(zkAppPublicKey, data1).toString();
+const verifiedData2 = signature.verify(zkAppPublicKey, data2).toString();
+
+c(`Private key: ${zkAppPrivateKey.toBase58()}`);
+c(`Public key: ${zkAppPublicKey.toBase58()}`);
+
+c(`Fields in private key: ${zkAppPrivateKey.toFields().length}`);
+c(`Fields in public key: ${zkAppPublicKey.toFields().length}`);
+
+c(`Signature verified for data 1: ${verifiedData1}`);
+c(`Signature verified for data 2: ${verifiedData2}`);
+
+c(`Fields in signature: ${signature.toFields().length}`);
+
+await shutdown();
